98aaacd3dfc982e73cd072bac97663c018b507c7
Jacob_Huebert <Jacob_Huebert@gallup.com>
2024-01-08 10:09:04 -0600

    Add project files.

---
+++ NavigationManagerTest/Authorization/TestAuthorizationHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace NavigationManagerTest.Authorization
+{
+    public class TestAuthorizationHandler : AuthorizationHandler<TestAuthorizationRequirement>
+    {
+        private readonly NavigationManager _navigationManager;
+
+        public TestAuthorizationHandler(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, TestAuthorizationRequirement requirement)
+        {
+            // If navigation happens internally and from links on the page, the navigation manager is initialized properly and retrieving the URI works fine.
+            // However, if the URL bar is manipulated manually or the page is refreshed by the user, the navigation manager is never initialized, and throws an exception when hitting EnsureInitialized when attempting to get the URI.
+
+            // should break here if the URL is manually entered instead of routing through the app
+            string _uri = this._navigationManager.Uri;
+
+            // Don't really care what happens
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NavigationManagerTest/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authorization;
+using NavigationManagerTest.Authorization;
+using NavigationManagerTest.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthorizationCore(options =>
+{
+    options.AddPolicy("test-policy", policy =>
+    {
+        policy.AddRequirements(new TestAuthorizationRequirement());
+    });
+});
+
+builder.Services.AddScoped<IAuthorizationHandler, TestAuthorizationHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
